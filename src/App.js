388b5a2e21fc3d43cf2539fcbb5abb9dc39f805d
eva <eva.marco@kaleidos.net>
2021-12-13 10:46:11 +0100

    Initial commit with react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Festín</h1>
+      <p>Una aplicación super guay de recetas</p>
+    </div>
+  );
+}
+
+export default App;
