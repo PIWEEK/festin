83afedb58e867c47a5fc715e99e54764cdc21d4a
eva <eva.marco@kaleidos.net>
2021-12-16 18:29:25 +0100

    remove console

--- src/routes/createRecipe/CreateRecipe.jsx
+++ src/routes/createRecipe/CreateRecipe.jsx
@@ -34,7 +34,6 @@ export default function CreateRecipe() {
         fetch(url)
             .then(response => response.json())
             .then((data) => {
-                console.log(data);
                 setSections(data);
                 setIsLoading(false);
             });
