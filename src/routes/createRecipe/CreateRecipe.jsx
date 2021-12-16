edd032d6ec4b1e4c6ab5688c54a3ff3435e07e68
eva <eva.marco@kaleidos.net>
2021-12-16 15:23:12 +0100

    add create section

--- src/routes/createRecipe/CreateRecipe.jsx
+++ src/routes/createRecipe/CreateRecipe.jsx
@@ -13,6 +13,7 @@ export default function CreateRecipe() {
     const [sections, setSections] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [choosedSection, setChoosedSection] = useState("");
+    const [shown, setShow] = useState(false);
 
     const baseUrl = `http://localhost:3004`;
     const fetchTags = (url) => {
@@ -50,10 +51,7 @@ export default function CreateRecipe() {
                 </main>
             );
     }
-    // const [thisRecipe] = recipe;
-    // const {name, tags, fav, ingredients, steps, credits, other, remember, rations } = thisRecipe;
-    // const ingredientList = Object.keys(ingredients);
-    // const stepList = Object.values(steps);
+    console.log(choosedSection)
     return (
         <>
         <nav className="navCreate">
@@ -74,10 +72,14 @@ export default function CreateRecipe() {
                 </span>
             </aside>
             <main className="recipeMain">
-                <div>
+                <div className="recipeHeader">
                     <p className="sectionsWrapper">
                         <span className="smallText">Tipo de receta {"  "}</span>
                         <span className="sectionsContainer">
+                            <label key="all" htmlFor="all">
+                                <input className="inputRadio" type="radio" onClick={() => setChoosedSection("")} name="sectionName" id="all" value="all" />
+                                <span className={`labelAsSection`} >Todas</span>
+                            </label>
                             {sections.map(section => {
                                     return <label key={section} htmlFor={section}>
                                         <input className="inputRadio" type="radio" onChange={() => setChoosedSection(section)} name="sectionName" id={section} value={section} />
@@ -90,25 +92,25 @@ export default function CreateRecipe() {
                     <section className={`FrontRecipeCreate ${choosedSection}`}>
                         <div className="PrepBar">
                             <div className="prepPartial">
-                                <p className={`prepText prepTitle`}>Preparación</p>
+                                <p className={`prepText prepTitle ${choosedSection}`}>Preparación</p>
                                 <p className={`prepText `}>
-                                    <input className="inputInvisible " placeholder="20 minutos" type="number" name="prep-time" id="prep" />
+                                    <input className="inputInvisible timeInput" placeholder="20 minutos" type="text" name="prep-time" id="prep" />
                                     </p>
                             </div>
                             <div className="prepPartial">
-                                <p className={`prepText prepTitle`}>Cocinado</p>
+                                <p className={`prepText prepTitle  ${choosedSection}`}>Cocinado</p>
                                 <p className={`prepText`}>
-                                    <input className="inputInvisible" placeholder="30 minutos" type="number" name="cook-time" id="cook" />
+                                    <input className="inputInvisible timeInput" placeholder="30 minutos" type="text" name="cook-time" id="cook" />
                                 </p>
                             </div>
                             <div className="prepPartial">
-                                <p className={`prepText prepTitle`}>Total</p>
+                                <p className={`prepText prepTitle  ${choosedSection}`}>Total</p>
                                 <p className={`prepText `}>
-                                    <input className="inputInvisible" placeholder="50 minutos" type="number" name="total-time" id="total" />
+                                    <input className="inputInvisible timeInput" placeholder="50 minutos" type="text" name="total-time" id="total" />
                                 </p>
                             </div>
                             <div className="prepPartial">
-                                <p className={`prepText prepTitle`}>Para</p>
+                                <p className={`prepText prepTitle  ${choosedSection}`}>Para</p>
                                 <p className={`prepText`}>
                                     <input className="inputInvisible rationInput" placeholder="4" type="number" name="rations" id="ration" />
                                     <span className="rationText">personas</span>
@@ -130,15 +132,51 @@ export default function CreateRecipe() {
                                     name="remember"
                                     id="remember"
                                     placeholder="Tu nota para no olvidar antes de empezar a cocinar"/>
-
-                            <p className="tagContainer">
-                                {tags.map(tag =>
-                                    <label htmlFor={tag}>
-                                        <input className="inputCheck" type="checkbox" name={tag} id={tag} />
-                                        <span className="labelAsButton" >{tag}</span>
-                                    </label>
-                                )}
-                            </p>
+                            <div className="tagsWrapper">
+                                <p className={`helpText  ${choosedSection}`}>Tags (selecciona las que quieras.)</p>
+                                <div className="tagContainerCreate">
+                                    <span
+                                        className="shownTags"
+                                        // style={{visibility:`${shown? "": "hidden"}`}}>
+                                            style={shown ? {visibility:"hidden", width: 0, height:0} : {}}>
+                                        <label key={tags[0]} htmlFor={tags[0]}>
+                                                    <input className="inputCheck" type="checkbox" name={tags[0]} id={tags[0]} />
+                                                    <span className={`labelAsButtonCreate  ${choosedSection}`} >{tags[0]}</span>
+                                        </label>
+                                        <label key={tags[1]} htmlFor={tags[1]}>
+                                                    <input className="inputCheck" type="checkbox" name={tags[1]} id={tags[1]} />
+                                                    <span className={`labelAsButtonCreate  ${choosedSection}`} >{tags[1]}</span>
+                                        </label>
+                                        <label key={tags[2]} htmlFor={tags[2]}>
+                                                    <input className="inputCheck" type="checkbox" name={tags[2]} id={tags[2]} />
+                                                    <span className={`labelAsButtonCreate  ${choosedSection}`} >{tags[2]}</span>
+                                        </label>
+                                        <button
+                                            style={{display:`${shown ? "none": "inline"}`}}
+                                            className="showMore"
+                                            onClick={() => setShow (true)}>Ver más
+                                        </button>
+                                    </span>
+                                    <span className="hiddenTags" style={shown ? {}: {visibility:"hidden", width: 0, height:0} }>
+                                        {tags.map((tag, index) =><label key={tag} htmlFor={tag}>
+                                                    <input className="inputCheck" type="checkbox" name={tag} id={tag} />
+                                                    <span className={`labelAsButtonCreate  ${choosedSection}`} >{tag}</span>
+                                                </label>)}
+                                        <label >
+                                            <input className="inputCheck" type="checkbox" name={tags[2]} id={tags[2]} />
+                                            <span className={`labelAsButtonCreate  ${choosedSection}`} >
+                                                <input  type="text" placeholder="Crear Nueva" />
+                                            </span>
+                                        </label>
+                                        <button
+                                            style={shown ? {}: {visibility:"hidden", width: 0, height:0} }
+                                            className="showLess"
+                                            onClick={() => setShow (false)}>
+                                                Ver menos
+                                        </button>
+                                    </span>
+                                </div>
+                            </div>
                         </div>
                     </section>
                 </div>
